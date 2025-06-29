9809042588722a6eede0ef5de40a97b2cd922231
Orima <amirsalimi1394@gmail.com>
2025-06-29 18:12:25 +0330

    Creating The Builder Class And Its Dependencies

--- DependencyInjectionApp/Program.cs
+++ DependencyInjectionApp/Program.cs
@@ -1,10 +1,42 @@
 ﻿namespace DependencyInjectionApp
 {
-    internal class Program
+    public class Hammer
     {
-        static void Main(string[] args)
+        public void Use()
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Using the hammer!");
+        }
+    }
+    public class Saw
+    {
+        public void Use()
+        {
+            Console.WriteLine("Using the saw!");
+        }
+
+    }
+    public class Builder
+    {
+        private Saw _saw;
+		private Hammer _hammer;
+        public Builder()
+        {
+            _saw = new Saw(); // Builder is responsible for creating its dependencies.
+			_hammer = new Hammer();//Builder is responsible for creating its dependencies.
+        }
+
+        public void BuildHouse()
+        {
+            _saw.Use(); // Builder uses its dependency. 
+			_hammer.Use(); // Builder uses its dependency.
+            Console.WriteLine("Building a house!");
+        }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Hello, World!");
+            }
         }
     }
 }
